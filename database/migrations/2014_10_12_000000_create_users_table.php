7698869c20797c806a3fabd8a70a6ed8d0599497
Nguyễn Lương Nam <35765669+nguyennamkkb@users.noreply.github.com>
2021-09-20 21:34:54 +0700

    qwqw

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',255);
+            $table->string('email',255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->string('code')->nullable();
+            $table->string('password',255)->nullable();
+            $table->string('code',6)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
